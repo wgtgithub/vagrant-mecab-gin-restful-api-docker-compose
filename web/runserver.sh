ee27be8d05b65e509386b17f5d59908f9e3c9bfc
wgtgithub <wgt19680715@gmail.com>
2020-09-11 21:18:10 +0900

    fixed runserver.sh scripts

--- web/runserver.sh
+++ web/runserver.sh
@@ -4,7 +4,7 @@
 cp -r /root/mecab-backup/lib/mecab /usr/local/lib/
 cp /root/mecab-backup/lib/libmecab.* /usr/local/lib/
 cp /root/mecab-backup/bin/* /usr/local/bin/
-ln -s /usr/local/lib/mecab/dic/mecab-ipadic-meologd /usr/local/lib/mecab/ipadic
+ln -s /usr/local/lib/mecab/dic/mecab-ipadic-neologd /usr/local/lib/mecab/dic/ipadic
 
 export CGO_LDFLAGS="`mecab-config --libs`"
 export CGO_CFLAGS="-I`mecab-config --inc-dir`"
@@ -12,5 +12,9 @@ export CC=/usr/bin/clang
 
 go get -u github.com/gin-gonic/gin
 go get -u github.com/bluele/mecab-golang
+
+export CGO_LDFLAGS="`mecab-config --libs`"
+export CGO_CFLAGS="-I`mecab-config --inc-dir`"
+export CC=/usr/bin/clang
 
 go run ./src/
